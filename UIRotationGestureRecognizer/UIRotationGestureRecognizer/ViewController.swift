9ddfb959811820f3a6ec5b3a3464c150f2895095
MBP0037 <mbp0037@MBP0037.local>
2021-04-21 22:02:51 +0700

    UIRotationGestureRecognizer

---
+++ UIRotationGestureRecognizer/UIRotationGestureRecognizer/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  UIRotationGestureRecognizer
+//
+//  Created by MBP0037 on 4/21/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var rttImg: UIRotationGestureRecognizer!
+    @IBOutlet weak var imgLogo: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        rttImg.rotation = 0
+    }
+
+    
+    @IBAction func actionRTTImg(_ sender: UIRotationGestureRecognizer) {
+        imgLogo.transform = imgLogo.transform.rotated(by: sender.rotation)
+    }
+}
+
