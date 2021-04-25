ae92e5ce17f40948b2b99609ada2571f10cd1aa7
MBP0037 <mbp0037@MBP0037.local>
2021-04-25 23:37:26 +0700

    datePicker

---
+++ DatePicker/DatePicker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DatePicker
+//
+//  Created by MBP0037 on 4/25/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var datePicker: UIDatePicker!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let formatter: DateFormatter = DateFormatter()
+        formatter.dateFormat = "E d D M Y y / H h m s / A a"
+        print(formatter.string(from: datePicker.date))
+    }
+}
+
